a2e47409c8f87453314d6c57a5e32419914fc677
Richard Sanderson <richardj.sanderson@gmail.com>
2021-03-22 14:04:17 +0000

    new option rows

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,63 @@
 $(document).ready(function () {
 
-
+    
     var collectionGrid = '#shopify-section-collection-template .scroll-container';
     var collectionGridInner = collectionGrid + ' .scroll-container-inner';
-    var gridProduct = collectionGridInner + ' .product'
-    
+    var gridProduct = collectionGridInner + ' .product';
+    var isCollection = $('body').hasClass('template-collection');
     let 
         pagNext = '.custom-next a',
         pagHide = '.pag-hide',
         pagStatus = '.page-load-status';
     
-    $(collectionGridInner).infiniteScroll({
-        // options
-        path: pagNext,
-        append: gridProduct,
-        hideNav: pagHide,
-        scrollThreshold: 600,
-        status: pagStatus
-    });
 
-    $(collectionGridInner).on( 'request.infiniteScroll', function() {
-        $(pagHide).hide()
-        console.log('threshold')
-    });
+    if (isCollection) {
 
-    $(collectionGridInner).on( 'append.infiniteScroll', function (items) {
-        console.log('append');s
-    });
+        console.log('is a collection')
+
+        $(collectionGridInner).infiniteScroll({
+            // options
+            path: pagNext,
+            append: gridProduct,
+            hideNav: pagHide,
+            scrollThreshold: 600,
+            status: pagStatus
+        });
+    
+        $(collectionGridInner).on( 'request.infiniteScroll', function() {
+            $(pagHide).hide()
+            console.log('threshold')
+        });
+    
+        $(collectionGridInner).on( 'append.infiniteScroll', function (items) {
+            console.log('append');s
+        });
+    } else {
+        console.log('not a collection')
+    }
+
+    var optionSelector = 'select#print-selector';
+    var optionSelectorOption = optionSelector + ' option';
+
+    $(optionSelectorOption).each(function() {
+        if($(this).is(':selected')) {
+            $(this).addClass('selected-print')
+        }
+    })
+
+    $(optionSelector).on('change', function () {
+       
+        $(optionSelectorOption).each(function() {
+            if($(this).is(':selected')) {
+                $(this).addClass('selected-print')
+            } else {
+                $(this).removeClass('selected-print')
+            }
+        })
+
+    })
+       
+    
+
     
 });
